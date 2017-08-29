f6113949cc0995d6472776c967812acc83e2a3a0
animeshjn <animesh.jain@hotmail.com>
2017-08-29 10:16:27 -0400

    standard token created

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,10 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var animesh = artifacts.require("./Animesh.sol");
 var practice = artifacts.require("./practice.sol");
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-  deployer.deploy(practice);
+  deployer.link(ConvertLib, animesh);
+  //deployer.deploy(MetaCoin);
+  //deployer.deploy(practice);
 };
